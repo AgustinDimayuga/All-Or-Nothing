512ecd18485e9cc5134797a69e0970326a5a4cbb
Agustin Xocua Dimayuga <Axocuadi@calpoly.edu>
2026-05-01 10:41:25 -0700

    Load db from env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -9,6 +10,12 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option(
+    "sqlalchemy.url",
+    os.getenv(
+        "POSTGRES_URI", "postgresql+psycopg://myuser:mypassword@localhost/mydatabase"
+    ),
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -64,9 +71,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
