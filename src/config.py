b2483a2471d30f42854272b740acdeffe0ef50ce
Agustin Xocua Dimayuga <Axocuadi@calpoly.edu>
2026-05-01 10:30:13 -0700

    Added Override

--- src/config.py
+++ src/config.py
@@ -3,7 +3,8 @@ import os
 from functools import lru_cache
 
 load_dotenv(dotenv_path="default.env", override=False)
-load_dotenv(dotenv_path=find_dotenv(".env"), override=False)
+
+load_dotenv(dotenv_path=find_dotenv(".env"), override=True)
 
 
 class Settings:
