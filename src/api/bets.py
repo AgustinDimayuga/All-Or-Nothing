ba9a9efaffb6ec873189e95392b86251d3abcaf6
fabian2111 <limonespe5@gmail.com>
2026-05-04 19:57:05 -0700

    Made working bets

--- src/api/bets.py
+++ src/api/bets.py
@@ -1,18 +1,15 @@
-from datetime import date
-
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel, Field
 from typing import List, Annotated
 
 import sqlalchemy
-from starlette import status
+
+from src.api.user_helper import get_token_data
 
 from src import database as db
 
 router = APIRouter(
-    prefix="/bets",
-    tags=["bets"],
-    # dependencies=[], //Add JWT
+    prefix="/bets", tags=["bets"], dependencies=[Depends(get_token_data)]
 )
 
 
@@ -25,35 +22,47 @@ class Bet(BaseModel):
 class BetResponse(BaseModel):
     bet_id: int
     game_id: int
-    # game_summary: str  # might remove
     team_bet_on: str
     amount: float
     odds: float
     potential_payout: float
-    # status: str
+    status: str
     placed_at: str
-    # new_balance: float  # Might be unnecessary
+    new_balance: float  # Might be unnecessary
 
 
-@router.post("/create", status_code=status.HTTP_201_CREATED)
-def create_bet(user_id: int):
+# @router.post("/create", status_code=status.HTTP_201_CREATED)
+# def create_bet(user_id: int):
+#     with db.engine.begin() as connection:
+#         bet_id = connection.execute(
+#             sqlalchemy.text("""
+#                 INSERT INTO new_bets (user_id)
+#                 VALUES (:user_id)
+#                 RETURNING bet_id
+#                 """),
+#             [{"user_id": user_id}],
+#         ).scalar_one()
+#
+#     return bet_id
+
+
+@router.post("/place", response_model=BetResponse)
+def place_bet(user_id: int, new_bet: Bet):
     with db.engine.begin() as connection:
-        bet_id = connection.execute(
+
+        cur_balance = connection.execute(
             sqlalchemy.text("""
-                INSERT INTO new_bets (user_id)
-                VALUES (:user_id)
-                RETURNING bet_id
+                SELECT sum(change)
+                FROM wallet
+                WHERE user_id = :user_id
                 """),
             [{"user_id": user_id}],
         ).scalar_one()
 
-    return bet_id
+        if new_bet.amount > cur_balance:
+            raise HTTPException(status_code=422, detail="Not enough money")
 
-
-@router.post("/place", response_model=BetResponse)
-def place_bet(user_id: int, bet_id: int, new_bet: Bet):
-    with db.engine.begin() as connection:
-        odds = 2
+        odds = 2  # Hard code odds for now
 
         team_id = connection.execute(
             sqlalchemy.text("""
@@ -64,29 +73,49 @@ def place_bet(user_id: int, bet_id: int, new_bet: Bet):
             [{"name": new_bet.team}],
         ).scalar_one()
 
-        placed_at = connection.execute(
+        values = (
+            connection.execute(
+                sqlalchemy.text("""
+                INSERT INTO bets (user_id, game_id, team_id, odds, amount)
+                VALUES (:user_id, :game_id, :team_id, :odds, :amount)
+                RETURNING id, created_at
+                """),
+                [
+                    {
+                        "user_id": user_id,
+                        "game_id": new_bet.game_id,
+                        "team_id": team_id,
+                        "amount": new_bet.amount,
+                        "odds": odds,
+                    }
+                ],
+            )
+            .mappings()
+            .one()
+        )
+
+        connection.execute(
             sqlalchemy.text("""
-                INSERT INTO bets (user_id, game_id, team_id, amount, odds)
-                VALUES (:user_id, :game_id, :team_id, :amount, :odds)
-                RETURNING created_at
+                INSERT INTO wallet (user_id, from_bet, change)
+                VALUES (:user_id, :bet_id, :change)
                 """),
             [
                 {
                     "user_id": user_id,
-                    "game_id": new_bet.game_id,
-                    "team_id": team_id,
-                    "amount": new_bet.amount,
-                    "odds": odds,
+                    "bet_id": values["id"],
+                    "change": new_bet.amount * -1,
                 }
             ],
-        ).scalar_one()
+        )
 
         return BetResponse(
-            bet_id=bet_id,
+            bet_id=values["id"],
             game_id=new_bet.game_id,
             team_bet_on=new_bet.team,
             amount=new_bet.amount,
             odds=odds,
             potential_payout=odds * new_bet.amount,
-            placed_at=placed_at,
+            status="active",
+            placed_at=str(values["created_at"]),
+            new_balance=cur_balance - new_bet.amount,
         )
