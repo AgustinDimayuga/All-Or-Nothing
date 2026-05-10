ca7dbbc0b21b45da7ce45882c30065fa3f9dcbcb
Migue654 <mmedin52@calpoly.edu>
2026-05-10 11:35:42 -0700

    Flow2-CronJob

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ class Settings:
     POSTGRES_URI: str | None = os.getenv("POSTGRES_URI")
     SECRET_KEY: str | None = os.getenv("SECRET_KEY")
     ALGORITHM: str | None = os.getenv("ALGORITHM")
-    NHL_API_KEY: str | None = os.getenv("NHL_KEY")
+
 
     def __init__(self):
         if not self.API_KEY:
@@ -23,8 +23,7 @@ class Settings:
             raise ValueError("SECRET_KEY is missing.")
         if not self.ALGORITHM:
             raise ValueError("ALGORITHM is missing.")
-        if not self.NHL_API_KEY:
-            raise ValueError("NHL API KEY is missing")
+
 
 
 @lru_cache()
